700006b3d13e6a8959e2138c8e5592a1841205ce
wkurmann <willy.kurmann@gmail.com>
2017-11-06 15:33:49 +0100

    User input

---
+++ letsread.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Type a string and press enter:"
+read input
+echo "you entered: $input"
---
+++ sentence.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Enter an adjective, a noun and a verb:"
+read adjective
+read noun
+read verb
+echo "$noun $verb $adjective"
+
